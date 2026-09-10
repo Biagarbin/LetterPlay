4249fd40c14e244abcc999fb2f597653ee21f398
ellyzGon <elly.santosgoncalves00@gmail.com>
2026-09-10 15:38:47 -0300

    correção

--- src/App.tsx
+++ src/App.tsx
@@ -11,15 +11,12 @@ import AlunoLogin from './pages/Aluno/AlunoLogin'
 import FormeAPalavra from './pages/Aluno/Jogos/FormeAPalavra'
 
 import CorridaDasPalavras from './pages/Aluno/Jogos/CorridaDasPalavras'
-<<<<<<< HEAD
 import Matematica from './pages/Aluno/Jogos/Matematica' 
 import Cruzadinha from './pages/Aluno/Jogos/Cruzadinha'
 import ContaEEscreve from './pages/Aluno/Jogos/ContaEscreve'
-=======
 import QuizdeOrtografia from './pages/Aluno/Jogos/QuizdeOrtografia'
 import ImagemEPalavra from './pages/Aluno/Jogos/ImagemePalavra'
 import DesenhoLivre from './pages/Aluno/Jogos/DesenhoLivre'
->>>>>>> ab75e2bdbaa3c6cd0d6333e2047feaab04ee3b82
 
 
 function App() {
@@ -65,7 +62,7 @@ function App() {
         
          <Route
           path="/Aluno/Jogos/QuizdeOrtografia"
-          element={<QuizdeOrtografia />}
+          element={<QuizDeOrtografia />}
 
         />
 
